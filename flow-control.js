f12fe4bb35cd86fd47184cc600ce2a31bdf1410a
Jay Friedmann <jay@swissmediainc.net>
2019-03-07 22:37:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if (age > 13 && age < 19) {
+  if (age > 12 && age < 20) {
     return "You are a teenager!"
   } else {
     return "You are not a teenager"
@@ -13,9 +13,9 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age > 13 && age < 19) {
+  if (age > 12 && age < 20) {
     return "You are a teenager!"
-  } else if (age < 12){
+  } else if (age < 13){
     return "You are a kid"
   } else {
     return "You are a grownup"
