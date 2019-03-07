113c11b60b5c6baa901a48ed4cf5d851ef39be34
Jay Friedmann <jay@swissmediainc.net>
2019-03-07 22:50:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,8 +27,8 @@ function ternaryTeenager(age) {
   return age > 12 && age < 20 ? "You are a teenager" :  "You are not a teenager"
 }
 
-function switchAge("age") {
-  switch("age") {
+function switchAge(age) {
+  switch(age) {
       case 13:
 console.log("You are a teenager");
         break;
